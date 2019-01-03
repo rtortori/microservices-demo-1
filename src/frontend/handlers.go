7ccb5423b37f91ef5af21a31a73ea9d348a4705e
blipzimmerman <blip.zimmerman@gmail.com>
2019-01-02 19:10:56 -0500

    Changed repeat format

--- src/frontend/handlers.go
+++ src/frontend/handlers.go
@@ -194,19 +194,15 @@ func (fe *frontendServer) viewCartHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 	
-	var (
-		loopCount
-		num_recommendations
-		recommendations
-	    )
-	for num_recommendations <= 4 {
-		if loopCount > 3 { break } //only attempt 3 times to get more results
+	loopCount := 0
+	recommendations, err := fe.getRecommendations(r.Context(), sessionID(r), cartIDs(cart))	
+	for len(recommendations) <= 4 {
+		if loopCount > 2 { break } //only attempt 3 times to get more results
 		recommendations, err = fe.getRecommendations(r.Context(), sessionID(r), cartIDs(cart))	
 		if err != nil {
 			renderHTTPError(log, r, w, errors.Wrap(err, "failed to get product recommendations"), http.StatusInternalServerError)
 			return
 		}
-		num_recommendations = len(recommendations)
 		loopCount += loopCount
 	}	
 
